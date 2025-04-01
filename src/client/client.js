c003645616f19fc92feffebbb544baf0b954dca1
The Gentlehacker <github@dylanlacey.com>
2025-04-01 22:26:43 +1000

    Finish step 1 for Getting Started

--- src/client/client.js
+++ src/client/client.js
@@ -1,2 +1,51 @@
 // Client-side JavaScript entry point
 console.log('Client-side JavaScript loaded'); 
+
+import { connectToGumnut, buildTestToken } from 'gumnut-v0-api';
+import 'gumnut-v0-api/dom';
+
+console.log("Creating new AbortController");
+const controller = new AbortController();
+
+var gumnut;
+
+try {
+    gumnut = connectToGumnut(controller.signal, {
+        projectId: 'good-dogs',
+        localDevKey: '_DO_NOT_USE_IN_PROD_UWi5Yh2QJntqDwZlTToekA',
+        host: "v0-collab.dev.gumnut.dev",
+    });
+
+    const token = buildTestToken('bandit-h', {
+        name: 'Bandit',
+        email: 'bandit.h@gumnut.dev'
+    });
+    console.log("Built token", token);
+    
+    gumnut.provideToken(token);
+
+    const doc = gumnut.join(controller.signal, 'Spot');
+    await doc.ready;
+    console.log("Document ready", doc);
+
+    const dogNameNode = doc.forNode('dogName')
+    document.getElementById('dogName').node = dogNameNode;
+
+    const descriptionNode = doc.forNode('description')
+    document.getElementById('description').node = descriptionNode;
+
+    const floppyEarsNode = doc.forNode('floppyEars')
+    document.getElementById('floppyEars').node = floppyEarsNode;
+
+    const boopableSnootNode = doc.forNode('boopableSnoot')
+    document.getElementById('boopableSnoot').node = boopableSnootNode;
+
+    const toeBeansNode = doc.forNode('toeBeans')
+    document.getElementById('toeBeans').node = toeBeansNode;
+
+    const goodDogRangeNode = doc.forNode('goodDogRange')
+    document.getElementById('goodDogRange').node = goodDogRangeNode;
+
+} catch (error) {
+    console.error("Error connecting to Gumnut", error);
+}
