039c900f48f6e3671bed20cc7c74b3938b73bf0b
The Gentlehacker <github@dylanlacey.com>
2025-04-04 16:25:40 +1000

    Update API to 0.2.3

--- src/client/client.js
+++ src/client/client.js
@@ -1,32 +1,23 @@
 // Client-side JavaScript entry point
 console.log('Client-side JavaScript loaded'); 
 
-import { connectToGumnut, buildTestToken } from 'gumnut-v0-api';
+import { connectToGumnutDoc, buildTestToken, useNode } from 'gumnut-v0-api';
 import 'gumnut-v0-api/dom';
 
-console.log("Creating new AbortController");
-const controller = new AbortController();
+// console.log("Creating new AbortController");
+// const controller = new AbortController();
 
 var gumnut;
 
 try {
-    gumnut = connectToGumnut(controller.signal, {
-        projectId: 'good-dogs',
-        localDevKey: '_DO_NOT_USE_IN_PROD_UWi5Yh2QJntqDwZlTToekA',
-        host: "v0-collab.dev.gumnut.dev",
-    });
-
     const token = buildTestToken('bandit-h', {
         name: 'Bandit',
         email: 'bandit.h@gumnut.dev'
     });
-    console.log("Built token", token);
-    
-    gumnut.provideToken(token);
 
-    const doc = gumnut.join(controller.signal, 'Spot');
+    const doc = connectToGumnutDoc({docId: "good-dogs", getToken: () => token}).doc
     await doc.ready;
-    console.log("Document ready", doc);
+    console.log(`Ready to edit "${doc.docId}" with Gumnut.dev`);
 
     const dogNameNode = doc.forNode('dogName')
     document.querySelectorAll('[name="dogName"]').forEach(element => {
@@ -54,15 +45,9 @@ try {
 
     document.getElementById('save').addEventListener('click', () => {
         doc.actions.commit(async (changes) => { })
-
         console.log('Document snapshot created'); 
     });
 
 } catch (error) {
     console.error("Error connecting to Gumnut", error);
-}
-
-function saveDocument() {
-    doc.snapshot();
-    console.log('Document snapshot created');
 }
