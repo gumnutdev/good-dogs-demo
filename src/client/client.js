d2256757df0629e439ff62f9d967c49035bf9066
Owen Brasier <owen@gumnut.dev>
2025-04-07 11:03:57 +1000

    dumb error

--- src/client/client.js
+++ src/client/client.js
@@ -1,2 +1,33 @@
 // Client-side JavaScript entry point
-console.log('Client-side JavaScript loaded'); 
+console.log("Client-side JavaScript loaded");
+
+import {
+  connectToGumnutDoc,
+  configureGumnut,
+  buildTestToken,
+} from "gumnut-v0-api";
+
+import "gumnut-v0-api/dom";
+
+// Configure Gumnut
+configureGumnut({
+  remoteHost: "v0-collab.dev.gumnut.dev",
+  projectId: "love-doggos",
+  localDevKey: "_DO_NOT_USE_IN_PROD_ji8aimPL0CeTUmQfYXr6Gg",
+});
+
+const doc = await connectToGumnutDoc({
+  docId: "not-different-enough-dogs",
+  getToken: () =>
+    buildTestToken("bandit-h", {
+      name: "Bandit",
+      email: "bandit.h@gumnut.dev",
+    }),
+});
+
+await doc.ready;
+console.log("Doc is ready", doc.docId);
+
+document.getElementById("dogName").node = doc.forNode("dogName");
+
+console.log("Gumnut loaded");
