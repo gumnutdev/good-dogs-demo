e64d1bc211c10fe58d53fa4b20a025cb87b7daa1
Owen Brasier <owen@gumnut.dev>
2025-05-08 11:52:31 +1000

    simplified html

--- src/client.js
+++ src/client.js
@@ -1,17 +1,1 @@
-// Client-side JavaScript entry point
-console.log("Client-side JavaScript loaded");
 
-// Update slider background based on value
-const slider = document.querySelector('input[type="range"]');
-const updateSliderBackground = (value) => {
-  const percentage = ((value - slider.min) / (slider.max - slider.min)) * 100;
-  slider.style.background = `linear-gradient(to right, var(--primary-color) 0%, var(--primary-color) ${percentage}%, var(--border-color) ${percentage}%, var(--border-color) 100%)`;
-};
-
-// Set initial background
-updateSliderBackground(slider.value);
-
-// Update background on input
-slider.addEventListener("input", (e) => {
-  updateSliderBackground(e.target.value);
-});
